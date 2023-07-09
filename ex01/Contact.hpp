029444543e376256604387ba8838de2b3461a092
aporlo <aporloporne@mail.com>
2023-07-09 14:10:45 +0700

    create contact and phonebook

---
+++ ex01/Contact.hpp
@@ -0,0 +1,28 @@
+#ifndef CONTACT_H
+# define CONTACT_H
+#include <string>
+
+
+# define RED		"\033[0;31m"
+# define GREEN		"\033[0;32m"
+# define YELLOW		"\033[0;33m"
+# define BLUE		"\033[0;34m"
+# define MAGENTA	"\033[0;35m"
+# define CYAN		"\033[0;36m"
+# define GRAY		"\033[0;37m"
+# define LRED		"\033[0;91m"
+# define BOLD		"\033[1m"
+# define GBOLD		"\033[1;37m"
+# define RESET		"\033[0m"
+
+class Contact {
+
+	public:
+		std::string fname;
+		std::string lname;
+		std::string nname;
+		std::string phone;
+		std::string darkest_s;
+
+
+}
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,67 +1,78 @@
 #include <iostream>
 
+std::string getcommand() {
+    std::string	command;
+
+	std::cout << "please enter your command: ";
+	std::getline(std::cin, command);
+	if (std::cin.eof()) {
+		exit(0);
+	}
+	return command;
+}
+
 class Contacts
 {
-    char firstname[100];
-    char lastname[100];
-    char nickname[50];
-    char phone[10];
-    char darkest_s[100];
+	public:
+    	std::string fname;
+		std::string lname;
+		std::string nname;
+		std::string phone;
+		std::string darkest_s;
+
+	Contacts(std::string fname, std::string lname, std::string nname, std::string phone, std::string darkest_s)
+	{
+		this->fname = fname;
+		this->lname = lname;
+		this->nname = nname;
+		this->phone = phone;
+		this->darkest_s = darkest_s;
+	}
+
 
     public:
         void set_contact()
         {
             std::cout <<"enter firstname:";
-            std::cin.getline(firstname, 100);
+			std::getline(std::cin, fname);
             std::cout <<"enter lastname:";
-            std::cin.getline(lastname, 100);
+            std::getline(std::cin, lname);
             std::cout <<"enter nickname:";
-            std::cin.getline(nickname, 50);
+            std::getline(std::cin, nname);
             std::cout <<"enter phone number:";
-            std::cin.getline(phone, 100);
+            std::getline(std::cin, phone);
             std::cout <<"enter darkest secret:";
-            std::cin.getline(darkest_s, 100);
+            std::getline(std::cin, darkest_s);
         }
 
         void display_contact()
         {
-            std::cout <<firstname<<"\t"<<lastname<<"\t"<<nickname<<"\t" << std::endl;
+            std::cout <<fname<<"\t"<<lname<<"\t"<<nname<<"\t"<<phone<<"\t"<<darkest_s<<"\t" << std::endl;
         }
 };
 
 int main()
 {
-    Contacts ph;
+    Contacts Contacts;
     std::string command;
 
-    std::cout << "please enter your command: ";
-    std::getline(std::cin >> std::ws, command);
-
-    if (command != "EXIT")
-    {
-        if (command == "ADD")
-        {
-            ph.set_contact();
-            ph.display_contact();
-        }
-        else if (command == "SEARCH")
-            std::cout << "SEARCH" << std::endl;
-        else
-            std::cout << "command must be ADD , SEARCH , EXIT" << std::endl;
+    // std::cout << "please enter your command: ";
+    // std::getline(std::cin >> std::ws, command);
+	while (true) {
+		command = getcommand();
+		if (command.empty())
+			continue;
+		if (command == "EXIT") {
+			std::cout << "bye bye" << std::endl;
+			break;
+		} else if (command == "ADD") {
+            Contacts.set_contact();
+        } else if (command == "SEARCH") {
+			 std::cout << "SEARCH" << std::endl;
+			 Contacts.display_contact();
+		} else {
+			std::cout << "command must be ADD , SEARCH , EXIT" << std::endl;
+		}
     }
-    else
-        std::cout << "bye bye" << std::endl;
-    // std::cout << "firstname: ";
-    // std::cin >> firstname;
-    // std::cout << "lastname: ";
-    // std::cin >> lastname;
-    // // std::cout << "lastname: ";
-    // // std::cin >> lastname << std::endl; 
-    // // std::cout << "nickname: ";
-    // // std::cin >> nickname;
-    // std::cout << " 1 " << firstname << std::endl;
-    // std::cout << " 2 " << lastname << std::endl;
-    // // std::cout << nickname << std::endl;
-    
     return 0;
 }
